e9e0490a446ed6a59b1146d10ee9cdc025629a07
Philippe Gauthier <philippe.gauthier@deuxpi.ca>
2012-03-30 15:24:32 -0400

    Implement receiving Modbus messages

--- pysunrise/modbus.py
+++ pysunrise/modbus.py
@@ -22,7 +22,7 @@ import struct
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from pymodbus.factory import ClientDecoder
 from pymodbus.transaction import ModbusBinaryFramer
-from pymodbus.utilities import computeCRC
+from pymodbus.utilities import checkCRC, computeCRC
 
 import logging
 logging.basicConfig()
@@ -30,6 +30,7 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 __all__ = ["InverterModbusClient"]
+
 
 class InverterModbusFramer(ModbusBinaryFramer):
     """This class builds Modbus frames (ADU) which are compatible with
@@ -42,8 +43,49 @@ class InverterModbusFramer(ModbusBinaryFramer):
 
     def __init__(self, decoder):
         ModbusBinaryFramer.__init__(self, decoder)
+        self.__buffer = ''
+        self.__header = {'crc': 0x0000, 'len': 0, 'uid': 0x00}
+        self.__hsize = 0x02
         self.__start = '\x0a'
         self.__end = '\x0d'
+
+    def checkFrame(self):
+        start = self.__buffer.find(self.__start)
+        if start == -1:
+            return False
+        self.__buffer = self.__buffer[start:]
+        end = self.__buffer.rfind(self.__end)
+        if end == -1:
+            return False
+        if end - start < 3:
+            return False
+        self.__header['len'] = end - start + 1
+        self.__header['uid'] = struct.unpack('>B', self.__buffer[start + 1])
+        self.__header['crc'] = struct.unpack('>H',
+                                             self.__buffer[end - 2:end])[0]
+        data = self.__buffer[start + 1:end - 2]
+        return checkCRC(data, self.__header['crc'])
+
+    def advanceFrame(self):
+        self.__buffer = self.__buffer[self.__header['len']:]
+        self.__header = {'crc': 0x0000, 'len': 0, 'uid': 0x00}
+
+    def isFrameReady(self):
+        return len(self.__buffer) > 1
+
+    def addToFrame(self, message):
+        self.__buffer += message
+
+    def getFrame(self):
+        start = self.__hsize + 1
+        end = self.__header['len'] - 3
+        buffer = self.__buffer[start:end]
+        if end > 0:
+            return buffer
+        return ''
+
+    def populateResult(self, result):
+        result.unit_id = self.__header['uid']
 
     def buildPacket(self, message):
         packet = struct.pack('>BB',
@@ -53,8 +95,8 @@ class InverterModbusFramer(ModbusBinaryFramer):
         packet = '%s%s%s' % (self.__start, packet, self.__end)
         return packet
 
+
 class InverterModbusClient(ModbusClient):
     def __init__(self, **kwargs):
         ModbusClient.__init__(self, method='binary', **kwargs)
         self.framer = InverterModbusFramer(ClientDecoder())
-
--- test/test_modbus.py
+++ test/test_modbus.py
@@ -4,24 +4,74 @@ from pysunrise.modbus import InverterModbusClient, InverterModbusFramer
 from pymodbus.pdu import ModbusRequest
 from pymodbus.factory import ServerDecoder
 
-class InverterClientTest(unittest.TestCase):
+
+class ModbusTest(unittest.TestCase):
+    def setUp(self):
+        self.framer = InverterModbusFramer(ServerDecoder())
+
     def testInverterClientInstantiation(self):
         client = InverterModbusClient()
         self.assertIsNotNone(client)
         self.assertIsInstance(client.framer, InverterModbusFramer)
 
-    def testInverterFramerPacket(self):
-        framer = InverterModbusFramer(ServerDecoder())
+    def testInverterFramerBufferEmpty(self):
+        self.assertFalse(self.framer.isFrameReady())
+        self.assertFalse(self.framer.checkFrame())
+
+    def testFramerLineFeedInMessage(self):
+        message = '\x0a\x01\x03\x02\x00\x0d\x79\x81\x0d'
+        self.framer.addToFrame(message)
+        self.assertTrue(self.framer.isFrameReady())
+        self.assertTrue(self.framer.checkFrame())
+        result = self.framer.getFrame()
+        self.assertEqual(message[3:-3], result)
+
+    def testFramerIncompleteMessage(self):
+        message = '\x0a\x01\x03'
+        self.framer.addToFrame(message)
+        self.assertTrue(self.framer.isFrameReady())
+        self.assertFalse(self.framer.checkFrame())
+
+    def testFramerMessageTooShort(self):
+        message = '\x0a\x0d'
+        self.framer.addToFrame(message)
+        self.assertTrue(self.framer.isFrameReady())
+        self.assertFalse(self.framer.checkFrame())
+
+    def testInverterFramerDecoding(self):
+        message = '\x0a\x01\x03\x02\x00\xcb\xf9\xd3\x0d'
+        self.framer.addToFrame(message)
+        self.assertTrue(self.framer.isFrameReady())
+        self.assertTrue(self.framer.checkFrame())
+        result = self.framer.getFrame()
+        self.assertEqual(message[3:-3], result)
+        self.framer.advanceFrame()
+        self.assertFalse(self.framer.isFrameReady())
+        self.assertFalse(self.framer.checkFrame())
+        self.assertEqual('', self.framer.getFrame())
+
+    def testInverterFramerDecodeTwice(self):
+        message = '\x0a\x01\x03\x02\x00\xcb\xf9\xd3\x0d'
+        self.framer.addToFrame(message)
+        self.assertTrue(self.framer.isFrameReady())
+        self.assertTrue(self.framer.checkFrame())
+        result = self.framer.getFrame()
+        self.assertEqual(message[3:-3], result)
+        self.framer.advanceFrame()
+        self.framer.addToFrame(message)
+        self.assertTrue(self.framer.isFrameReady())
+        self.assertTrue(self.framer.checkFrame())
+
+    def testInverterFramerEncoding(self):
         old_encode = ModbusRequest.encode
         ModbusRequest.encode = lambda self: ''
         message = ModbusRequest()
         message.unit_id = 0xff
         message.function_code = 0x01
         expected = '\x0a\xff\x01\x81\x80\x0d'
-        actual = framer.buildPacket(message)
+        actual = self.framer.buildPacket(message)
         ModbusRequest.encode = old_encode
         self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
     unittest.main()
-
